9e06753e858f3f4968669cf842d285bf94bedfea
tsl <taosiliang1127tsl@163.com>
2018-03-26 13:03:59 +0800

    加入cookie

--- server.js
+++ server.js
@@ -23,9 +23,43 @@ var server = http.createServer(function(request, response){
 
   if(path === '/'){
       let string = fs.readFileSync('./index.html','utf8')
+    //   var users=fs.readFileSync('./db/users','utf8')
+    //   try{
+    //       users=JSON.parse(users)
+    //   }catch{
+    //       users=[]
+    //   }
+      console.log(request.headers.cookie)
+      let cookies = request.headers.cookie.split('; ')  //把不同的用户分隔开['email=1@tsl.com','a=1','b=2']
+      let hash ={}
+      for(let i=0;i<cookies.length;i++){
+          let parts=cookies[i].split('=')
+          let key =parts[0]
+          let value = parts[1]
+          hash[key]=value
+      }
+      console.log(hash)
+      let email = hash.sign_in_email
+      let users = fs.readFileSync('./db/users','utf8')
+      users=JSON.parse(users)
+      let foundUser
+      for(i=0;i<users.length;i++)
+      {
+          if(users[i].email===email){
+            foundUser=users[i]
+            break
+          }
+      }
+
+      if(foundUser){
+          string = string.replace('__password__',foundUser.password)
+      }else{
+        string = string.replace('__password__','不知道')
+      }
+
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write('string')
+    response.write(string)
     response.end()
   }else if(path==='/sign_up'&& method==='GET'){
     let string = fs.readFileSync('./sign_up.html','utf8')    
@@ -44,16 +78,20 @@ var server = http.createServer(function(request, response){
             let parts=string.split('=')  //['email','1'],
             let key=parts[0]
             let value=parts[1]
-            hash[key]=value  //hash['email']='1'
+            // hash[key]=value  //hash['email']='1'
+            hash[key]=decodeURIComponent(value)
         })
         // console.log(hash)
         // let email=hash['email']
         // let password=hash['password']
         // let password_confirmation = hash['password_confirmation']
         let {email,password,password_confirmation}=hash  //es6语法
+        // console.log(email)
+        // console.log(email.indexOf('@'))
         if(email.indexOf('@')===-1){   //找不到@的话
         response.statusCode = 400  
         // response.write('email is bad')  
+        response.setHeader('Content-Type','application/json;charset=utf-8')
         response.write(`{
             "errors":{
                 "email":"invalid"
@@ -63,8 +101,33 @@ var server = http.createServer(function(request, response){
             response.statusCode = 400  
             response.write('password not match')  
         }else{
-         response.statusCode = 200
-        
+            var users=fs.readFileSync('./db/users','utf8')
+            try{
+            users=JSON.parse(users)  //[]            
+            }catch(exception){
+                users=[]
+            }
+            let inUse= false
+            for(let i=0;i<users.length;i++){
+                let user =  users[i]
+                if(user.email===email){
+                    inUse=true  //已经有用户注册过了
+                    break;
+                }
+            }
+            if(inUse){
+                response.statusCode = 400 
+                response.write('email in use')  
+            
+            }
+            else{
+                users.push({email:email,password:password})   //是个对象
+                var usersString =   JSON.stringify(users)
+                fs.writeFileSync('./db/users',usersString)  //文件只能存字符串
+                response.statusCode = 200
+            }
+            
+            
         }
         response.end()
     },()=>{
@@ -81,6 +144,53 @@ var server = http.createServer(function(request, response){
     //     response.statusCode = 200
     //     response.end()
     // })   
+  }else if(path==='/sign_in' && method==='GET'){  //请求页面
+    let string = fs.readFileSync('./sign_in.html','utf8')
+    response.statusCode=200
+    response.setHeader('Content-Type','text/html;charset=utf-8')
+    response.write(string)
+    response.end()
+  }else if(path==='/sign_in' && method==='POST'){   //提交表单
+    readyBody(request).then((body)=>{
+        // console.log(body)
+        let hash={}
+       let strings= body.split('&')  // ['email=1','password=2']
+        strings.forEach((string)=>{   //计数排序
+            //string=['email=1']
+            let parts=string.split('=')  //['email','1'],
+            let key=parts[0]
+            let value=parts[1]
+            // hash[key]=value  //hash['email']='1'
+            hash[key]=decodeURIComponent(value)
+        })
+        // console.log(hash)
+        // let email=hash['email']
+        // let password=hash['password']
+        let {email,password}=hash  //es6语法   ,用户输入的数据存起来
+        console.log(email)
+        console.log(password)   
+        var users = fs.readFileSync('./db/users','utf8')   //读取数据库里的数据
+        try{
+            users = JSON.parse(users)
+        }catch(exception){
+            users=[]
+        }
+        let found
+        for(let i=0;i<users.length;i++){
+            if(users[i].email===email && users[i].password===password){
+                found=true
+                break
+            }           
+        }
+        if(found){
+            response.setHeader('Set-Cookie',`sign_in_email=${email};HttpOnly`)  //设置cookie
+            response.statusCode=200
+        }else{
+            response.statusCode=401
+        }
+
+    response.end()    
+    })
   }
   else{
     response.statusCode = 404
